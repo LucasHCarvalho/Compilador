cce7ffda68ebe1ddc0cb6cee5acdae438ae428ab
Lucas Henrique <61765824+LucasHCarvalho@users.noreply.github.com>
2023-04-02 15:50:47 -0300

    inseindo função para identificação de numeros

--- lexer03/main.c
+++ lexer03/main.c
@@ -1,5 +1,8 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define LENGTH 512
 
 typedef enum Token {
   Mais,
@@ -8,15 +11,22 @@ typedef enum Token {
   Potencia,
   Divisao,
   Indeterminado,
+  Inteiro,
+  Decimal,
   Nulo
 } Token;
 
 typedef struct Simbolo {
   Token token;
-  char texto[2];
+  char texto[LENGTH];
 } Simbolo;
 
 typedef Simbolo *Indicador;
+
+void limpaTexto(Indicador token) {
+  for (int i = 0; i < LENGTH; i++)
+    token->texto[i] = NULL;
+}
 
 void imprimeToken(Indicador token) {
   if (token->token != Nulo) {
@@ -26,9 +36,26 @@ void imprimeToken(Indicador token) {
                        : (token->token == Potencia)      ? "Potencia"
                        : (token->token == Divisao)       ? "Divisao"
                        : (token->token == Indeterminado) ? "Indeterminado"
-                                                        : "");
-    printf("%c%c\n", token->texto[0], token->texto[1]);
+                       : (token->token == Inteiro)       ? "Inteiro"
+                       : (token->token == Decimal)       ? "Decimal"
+                                                         : "");
+    for (int i = 0; i < LENGTH; i++)
+      printf("%c", token->texto[i]);
+    printf("\n");
   }
+}
+
+int ultimoNumero(char *texto, int inicio, int tamanho) {
+  int final = inicio;
+
+  for (int i = inicio; i < tamanho; i++) {
+    if (isdigit(texto[i]))
+      final = i;
+    else
+      i = tamanho;
+  }
+
+  return final;
 }
 
 char tokenizar(char *texto, int tamanho) {
@@ -36,29 +63,56 @@ char tokenizar(char *texto, int tamanho) {
   novo = malloc(sizeof(Simbolo));
 
   for (int i = 0; i < tamanho; i++) {
-    novo->texto[1] = NULL;
+    limpaTexto(novo);
 
     if (texto[i] == ' ' || texto[i] == '\n' || texto[i] == '\0')
       novo->token = Nulo;
-    else if (texto[i] == '+')
+    else if (texto[i] == '+') {
       novo->token = Mais;
-    else if (texto[i] == '-')
+      novo->texto[0] = texto[i];
+    } else if (texto[i] == '-') {
       novo->token = Menos;
-    else if (texto[i] == '*') {
+      novo->texto[0] = texto[i];
+    } else if (texto[i] == '*') {
+      novo->texto[0] = texto[i];
       if (texto[i + 1] == '*') {
         novo->token = Potencia;
         novo->texto[1] = texto[i];
         i++;
       } else
         novo->token = Multiplicacao;
-    } else if (texto[i] == '/')
+    } else if (texto[i] == '/') {
       novo->token = Divisao;
-    else
+      novo->texto[0] = texto[i];
+    } else if (isdigit(texto[i])) {
+      novo->token = Inteiro;
+
+      int final = verificaNumero(texto, i, tamanho);
+
+      for (int j = i; j <= final; j++) {
+        novo->texto[j] = texto[j];
+      }
+
+      i = final;
+
+      if (texto[i + 1] == '.') {
+        i++;
+        novo->token = Decimal;
+        novo->texto[i] = texto[i];
+				
+        final = ultimoNumero(texto, i+1, tamanho);
+        for (int r = i; r <= final; r++) {
+          novo->texto[r] = texto[r];
+        }
+
+        i = final;
+      }
+    } else {
       novo->token = Indeterminado;
+      novo->texto[0] = texto[i];
+    }
 
-    novo->texto[0] = texto[i];
-
-		imprimeToken(novo);
+    imprimeToken(novo);
   }
 
   free(novo);
@@ -66,7 +120,7 @@ char tokenizar(char *texto, int tamanho) {
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    printf("Favor inserir 2 argumentos:\nEx.: main mbr.bin\n");
+    printf("Favor inserir 2 argumentos:\nEx.: texto.txt\n");
     return 1;
   }
 
